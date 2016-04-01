6fa07aa63340b25b723d3318a5a0a75d3df416e7
Arantis <arakalara@gmail.com>
2016-04-01 13:31:52 +0200

    fix some more translation stuff

--- blakserv/config.c
+++ blakserv/config.c
@@ -186,9 +186,9 @@ config_table_type config_table[] =
    
 { ADVERTISE_GROUP,        F, "[Advertise]",   CONFIG_GROUP, "" },
 { ADVERTISE_FILE1,        T, "File1",         CONFIG_STR,   "ad1.avi" },
-{ ADVERTISE_URL1,         T, "Url1",          CONFIG_STR,   "https://www.meridiannext.com/" },
+{ ADVERTISE_URL1,         T, "Url1",          CONFIG_STR,   "https://www.Meridian59.de/" },
 { ADVERTISE_FILE2,        T, "File2",         CONFIG_STR,   "ad2.avi" },
-{ ADVERTISE_URL2,         T, "Url2",          CONFIG_STR,   "http://openmeridian.org/forums" },
+{ ADVERTISE_URL2,         T, "Url2",          CONFIG_STR,   "http://www.wiki.Meridian59.de" },
 
 { DEBUG_GROUP,            F, "[Debug]",       CONFIG_GROUP, "" },
 { DEBUG_CANMOVEINROOM,    T, "CanMoveInRoom", CONFIG_BOOL,  "No" },
